83e140c45ca32843b50f4016b767ec61d8c8f448
eleonoravercesi <eleonora.vercesi@libero.it>
2024-11-22 11:24:10 +0100

    Adding files for vertices

--- vertices.py
+++ vertices.py
@@ -3,8 +3,46 @@ import gurobipy as gp
 from gurobipy import GRB
 from algorithms import JS_LP
 import fractions
+from math import ceil
+import subprocess
 
-def JS_LP_A_b(P, T, n_machines = None, verbose = False, fixed = []):
+
+def from_A_b_to_polymake_file(A, b, index_of_last_equality_constraint, out_filename):
+    F = open(out_filename, "w+")
+    F.write('use application "polytope";\nmy $p = new Polytope(EQUATIONS=>[')
+    n_constraints = A.shape[0]
+    n_vars = A.shape[1]
+    A_eq = A[:index_of_last_equality_constraint, :]
+    b_eq = b[:index_of_last_equality_constraint, :]
+    A_ineq = A[index_of_last_equality_constraint:, :]
+    b_ineq = b[index_of_last_equality_constraint:, :]
+
+    for k in range(A_eq.shape[0]):
+        F.write("[{}, ".format(str(-b_eq[k][0])))
+        contraint = ", ".join([str(A_eq[k, j]) for j in range(n_vars)])
+        F.write(contraint)
+        if k == A_eq.shape[0] - 1:
+            F.write("]")
+        else:
+            F.write("], ")
+
+    F.write('], INEQUALITIES=>[')
+    # Write constraints (inequalities)
+
+    for i in range(A_ineq.shape[0]):
+        F.write("[{}, ".format(str(-b_ineq[i][0])))
+        contraint = ", ".join([str(A_ineq[i, j]) for j in range(n_vars)])
+        F.write(contraint)
+        if i == n_constraints - 1:
+            F.write("]")
+        else:
+            F.write("], ")
+    F.write("]);\n")
+    F.write("print($p->VERTICES);")
+    F.close()
+
+
+def JS_LP_A_b(P, T, n_machines=None, verbose=False, fixed=[]):
     if n_machines == None:
         # You want to solve the problem "as it is"
         n_items, n_machines = P.shape
@@ -41,56 +79,37 @@ def JS_LP_A_b(P, T, n_machines = None, verbose = False, fixed = []):
 
     return m.getA(), m.getAttr('RHS')
 
-P = np.array([[48, 69],
-[26, 68],
-[84, 24]])
 
-obj, X = JS_LP(P)
-print(obj)
-from math import ceil
-obj = ceil(obj)
-A, b = JS_LP_A_b(P, obj)
-
-# From scipy sparse to numpy
-A = A.toarray()
-b = np.asarray(b).reshape(-1, 1)
-
-index_of_last_equality_constraint = P.shape[0]
-
-def from_A_b_to_polymake_file(A, b, index_of_last_equality_constraint, out_filename):
-        F = open(out_filename, "w+")
-        F.write('use application "polytope";\nmy $p = new Polytope(EQUATIONS=>[')
-        n_constraints = A.shape[0]
-        n_vars = A.shape[1]
-        A_eq = A[:index_of_last_equality_constraint, :]
-        b_eq = b[:index_of_last_equality_constraint, :]
-        A_ineq = A[index_of_last_equality_constraint:, :]
-        b_ineq = b[index_of_last_equality_constraint:, :]
-
-        for k in range(A_eq.shape[0]):
-                F.write("[{}, ".format(str(-b_eq[k][0])))
-                contraint = ", ".join([str(A_eq[k, j]) for j in range(n_vars)])
-                F.write(contraint)
-                if k == A_eq.shape[0] - 1:
-                        F.write("]")
-                else:
-                        F.write("], ")
-
-        F.write('], INEQUALITIES=>[')
-        # Write constraints (inequalities)
-
-        for i in range(A_ineq.shape[0]):
-                F.write("[{}, ".format(str(-b_ineq[i][0])))
-                contraint = ", ".join([str(A_ineq[i, j]) for j in range(n_vars)])
-                F.write(contraint)
-                if i == n_constraints - 1:
-                        F.write("]")
-                else:
-                        F.write("], ")
-        F.write("]);\n")
-        F.write("print($p->VERTICES);")
-        F.close()
+number_of_seeds = 1
+for seed in range(1):
+    np.random.seed(seed)
+    n_jobs = 3
+    n_machines = 2
+    P = np.random.randint(1, 100, (n_jobs, n_machines))
 
 
+    obj, X = JS_LP(P)
+    print(obj)
+    obj = ceil(obj)
+    A, b = JS_LP_A_b(P, obj)
 
-from_A_b_to_polymake_file(A,b, index_of_last_equality_constraint, out_filename="./output/vertices.pl")
+    # From scipy sparse to numpy
+    A = A.toarray()
+    b = np.asarray(b).reshape(-1, 1)
+
+    index_of_last_equality_constraint = P.shape[0]
+
+    from_A_b_to_polymake_file(A, b, index_of_last_equality_constraint, out_filename="./output/vertices.pl")
+
+    # Run polymake
+    subprocess.run("polymake --script ./output/vertices.pl > ./output/vertices.txt", shell=True)
+
+    # Parse the output
+    with open("./output/vertices.txt", "r") as f:
+        lines = f.readlines()
+
+    vertices = []
+
+    for line in lines:
+        line_vec = line.strip().split(" ")
+        for x in line_vec[1:]

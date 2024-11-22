932ce3be5263ee887bb801b894d17659da3307c9
eleonoravercesi <vercee@usi.ch>
2024-11-22 09:25:18 +0100

    Adding file for unrealted counterexample using vertices

---
+++ unrelated_experiment.py
@@ -0,0 +1,43 @@
+'''
+Just randomly generate some instances and solve their LP, to check that you can have two disjoint sets of jobs
+'''
+from algorithms import JS_LP
+import numpy as np
+from tqdm import tqdm
+
+TOL = 1e-6
+
+n = 3
+m = 2
+
+np.random.seed(7)
+def get_unfixed(X):
+    n, m = X.shape
+    unfixed = []
+    for i in range(n):
+        for j in range(m):
+            if abs(round(X[i, j]) - X[i, j]) > TOL:
+                unfixed.append(i)
+                break
+    return unfixed
+
+for _ in tqdm(range(1)):
+    P = np.random.randint(1, 100, (n, m))
+    obj, X_s = JS_LP(P)
+
+    unfixed = get_unfixed(X_s)
+
+
+    if len(unfixed) >= 1:
+        # Pick the first unfixed job
+        for i in unfixed:
+            unfixed_all = []
+            for j in range(m):
+                obj, X = JS_LP(P, fixed=[((i, j), 1)])
+                unfixed_all.append(get_unfixed(X))
+            if set(unfixed_all[0]).intersection(set(unfixed_all[1])) == set():
+                print('Found a disjoint set of jobs!!')
+                print(P)
+                print(X_s)
+                print(unfixed)
+                print(unfixed_all)
---
+++ vertices.py
@@ -0,0 +1,88 @@
+import numpy as np
+import gurobipy as gp
+from gurobipy import GRB
+from algorithms import JS_LP
+
+def JS_LP_A_b(P, T, n_machines = None, verbose = False, fixed = []):
+    if n_machines == None:
+        # You want to solve the problem "as it is"
+        n_items, n_machines = P.shape
+    else:
+        # Your machines are identical and you have m of them, hence, just copy the column vector T m times
+        try:
+            assert P.shape[1] == 1  # T is a column vector
+        except AssertionError:
+            raise ValueError("T must be a column vector in the case of identical machines")
+        # Copy the column vector T m times
+        P = np.tile(P, n_machines)
+        n_items = P.shape[0]
+    m = gp.Model("exact")
+    if not verbose:
+        m.setParam('OutputFlag', 0)
+        m.setParam('LogToConsole', 0)
+    # A binary variable x_i_j if variable i is assigned to machine j
+    x = m.addVars(n_items, n_machines, vtype=GRB.CONTINUOUS, name="x")
+    # Constraint: assign each job to exactly one machine
+    for i in range(n_items):
+        m.addConstr(sum(x[i, j] for j in range(n_machines)) == 1)
+
+    # Add the extra constraints
+    for (i, j), v in fixed:
+        m.addConstr(x[i, j] == v)  # v must be either 0 or 1
+
+    # Constraint: makespan is the maximum completion time
+    for j in range(n_machines):
+        m.addConstr(sum(x[i, j] * P[i, j] for i in range(n_items)) <= T)
+
+    return m.getA(), m.getAttr('RHS')
+
+P = np.array([[48, 69],
+[26, 68],
+[84, 24]])
+
+obj, X = JS_LP(P)
+A, b = JS_LP_A_b(P, obj)
+
+# From scipy sparse to numpy
+A = A.toarray()
+b = np.asarray(b).reshape(-1, 1)
+
+index_of_last_equality_constraint = P.shape[0] - 1
+
+def from_A_b_to_polymake_file(A, b, index_of_last_equality_constraint, out_filename):
+        F = open(out_filename, "w+")
+        F.write('use application "polytope";\nmy $p = new Polytope(EQUATIONS=>[')
+        n_constraints = A.shape[0]
+        n_vars = A.shape[1]
+        A_eq = A[:index_of_last_equality_constraint, :]
+        b_eq = b[:index_of_last_equality_constraint, :]
+        A_ineq = A[index_of_last_equality_constraint:, :]
+        b_ineq = b[index_of_last_equality_constraint:, :]
+
+        for k in range(A_eq.shape[0]):
+                F.write("[{}, ".format(str(int(-b_eq[k][0]))))
+                contraint = ", ".join([str(int(A_eq[k, j])) for j in range(n_vars)])
+                F.write(contraint)
+                if k == A_eq.shape[0] - 1:
+                        F.write("]")
+                else:
+                        F.write("], ")
+
+        F.write('], INEQUALITIES=>[')
+        # Write constraints (inequalities)
+
+        for i in range(A_ineq.shape[0]):
+                F.write("[{}, ".format(str(int(-b_ineq[i][0]))))
+                contraint = ", ".join([str(int(A_ineq[i, j])) for j in range(n_vars)])
+                F.write(contraint)
+                if i == n_constraints - 1:
+                        F.write("]")
+                else:
+                        F.write("], ")
+        F.write("]);\n")
+        F.write("print($p->VERTICES);")
+        F.close()
+
+
+
+from_A_b_to_polymake_file(A,b, index_of_last_equality_constraint, out_filename="./output/vertices.pl")
